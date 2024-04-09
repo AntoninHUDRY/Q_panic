da18684a5dc2f734166cc90e3359b2d071936732
AntoninHUDRY <antonin.hudry@epfl.ch>
2024-04-09 15:03:35 +0200

    added highlight to cubes

--- quantum/QuantumCube.gd
+++ quantum/QuantumCube.gd
@@ -10,7 +10,29 @@ var material: StandardMaterial3D
 @export
 var mesh: MeshInstance3D
 
+var highlighted = false
+@onready var outline = $RigidBody3D/MeshInstance3D/outline
+
 func _ready():
 	var surface_count = mesh.get_surface_override_material_count()
 	for i in range(surface_count):
 		mesh.set_surface_override_material(i, material)
+
+func _highlight() -> void :
+	highlighted = true
+	outline.visible = true
+
+func _unhighlight() -> void :
+	highlighted = false
+	outline.visible = false
+
+func _on_interactable_focused(interactor):
+	_highlight()
+
+
+func _on_interactable_interacted(interactor):
+	pass # Replace with function body.
+
+
+func _on_interactable_unfocused(interactor):
+	_unhighlight()

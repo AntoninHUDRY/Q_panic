d28e79629de7fa97ad33384bda0de3b9ab4601fb
Joel7815 <101325473+Joel7815@users.noreply.github.com>
2024-04-15 19:55:15 +0200

    Main Menu added
    Start game and quit works, still have to add options and pause menu

---
+++ Menu/main_menu.gd
@@ -0,0 +1,18 @@
+class_name MainMenu
+extends Control
+
+@export var main_game_packed_scene : PackedScene = null
+
+
+func _on_start_button_down():
+	get_tree().change_scene_to_packed(main_game_packed_scene)
+
+
+
+func _on_options_button_down():
+	pass # Replace with function body.
+
+
+
+func _on_quit_button_down():
+	get_tree().quit()

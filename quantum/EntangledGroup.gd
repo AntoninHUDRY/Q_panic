861df36a4770a27a7611af293c7a22a86c4e5f88
Yoric Züger <79338025+yzueger@users.noreply.github.com>
2024-04-16 14:11:04 +0200

    Make quantum behaviour more random and only happen when no quantum objects are in FOV

--- quantum/EntangledGroup.gd
+++ quantum/EntangledGroup.gd
@@ -4,7 +4,7 @@ class_name EntangledGroup
 
 var quantum_objects: Array[QuantumObject]
 var visible_object = 0
-var has_visible_been_seen = false
+var is_in_superposition = true
 
 func _ready():
 	quantum_objects = []
@@ -12,28 +12,31 @@ func _ready():
 		if obj is QuantumObject:
 			quantum_objects.append(obj)
 	print("Quantum objects: ", quantum_objects)
-	for i in range(1, len(quantum_objects)):
+	for i in range(0, len(quantum_objects)):
 		quantum_objects[i].quantum_hide()
 
 func set_new_visible(index: int):
 	print("Changing visible object from ", visible_object, " to ", index)
 	quantum_objects[visible_object].quantum_hide()
 	visible_object = index
-	has_visible_been_seen = false
 	quantum_objects[visible_object].quantum_show()
 
 # For now, simply ccolliderycle through the entangled objects that are not being observed
 func handle_observations(intersections: Array[Node3D]):
-	if quantum_objects[visible_object].collider in intersections:
-		has_visible_been_seen = true
-		return
+	var some_object_visible = false
 	
-	if not has_visible_been_seen:
-		return
-
-	for i in range(1, quantum_objects.size()):
-		var index = (i + visible_object) % quantum_objects.size()
-		if quantum_objects[index].collider not in intersections:
-			set_new_visible(index)
+	for i in range(0, quantum_objects.size()):
+		if quantum_objects[i].collider in intersections:
+			some_object_visible = true
 			break
+	
+	assert(some_object_visible == (len(intersections) != 0))
+	
+	if is_in_superposition and some_object_visible:
+		is_in_superposition = false
+		set_new_visible((randi() % (quantum_objects.size() - 1) + visible_object + 1) % quantum_objects.size() )
+	elif not is_in_superposition and not some_object_visible:
+		is_in_superposition = true
+		for i in range(0, len(quantum_objects)):
+			quantum_objects[i].quantum_hide()
 	

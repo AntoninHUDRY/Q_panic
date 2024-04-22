d68a4c804a5f8cba658f56f3f7e09d940663b779
Joel7815 <101325473+Joel7815@users.noreply.github.com>
2024-04-22 16:07:44 +0200

    Added Settings tab container
    No functionality, but visual there

--- Menu/main_menu.gd
+++ Menu/main_menu.gd
@@ -1,19 +1,36 @@
 class_name MainMenu
 extends Control
 
-@export var main_game_packed_scene : PackedScene = null
-@export var option_menu_packed_scene : PackedScene = null
+@onready var start_level = preload("res://CameraTest.tscn") as PackedScene
 
-
-func _on_start_button_down():
-	get_tree().change_scene_to_packed(main_game_packed_scene)
-
-
-
-func _on_options_button_down():
-	get_tree().change_scene_to_packed(option_menu_packed_scene)
+@onready var start_button = $MarginContainer/VBoxContainer/Start as Button
+@onready var options_button = $MarginContainer/VBoxContainer/Options as Button
+@onready var exit_button = $MarginContainer/VBoxContainer/Quit as Button
+@onready var options_menu = $Options_Menu as OptionsMenu
+@onready var margin_container = $MarginContainer as MarginContainer
 
 
 
-func _on_quit_button_down():
+func _ready():
+	handle_connecting_signals()
+
+func on_start_pressed() -> void:
+	get_tree().change_scene_to_packed(start_level)
+
+func on_options_pressed() -> void:
+	margin_container.visible = false
+	options_menu.set_process(true)
+	options_menu.visible = true
+
+func on_exit_pressed() -> void:
 	get_tree().quit()
+
+func on_exit_options_menu() -> void:
+	margin_container.visible = true
+	options_menu.visible = false
+
+func handle_connecting_signals() -> void:
+	start_button.button_down.connect(on_start_pressed)
+	options_button.button_down.connect(on_options_pressed)
+	exit_button.button_down.connect(on_exit_pressed)
+	options_menu.exit_options_menu.connect(on_exit_options_menu)

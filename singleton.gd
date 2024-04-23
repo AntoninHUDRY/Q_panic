d119a30dd273c01e20e55de0a77cd2ba8b530a9d
AntoninHUDRY <antonin.hudry@epfl.ch>
2024-04-23 14:30:51 +0200

    small changes

---
+++ singleton.gd
@@ -0,0 +1,8 @@
+extends Node
+#bienvenue dans le monde merveilleux des singleton. 
+
+#toute les variables inscrites ici sont accessibles dans n'importe quel noeud.
+#il suffit d'écrire Singleton.le_nom_de_la_variable
+
+var button_down = 0
+var expected_button_down = 3
